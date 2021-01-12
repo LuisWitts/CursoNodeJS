d00f6187f22b8b6a1903ecf753fe847e7d7a72ab
LuisWitts <57760175+LuisWitts@users.noreply.github.com>
2021-01-11 22:41:14 -0300

    Conceito de Módulos JS

---
+++ Aula001-Modulos/calc.js
@@ -0,0 +1,13 @@
+var SomaFunc = require("./somar");
+
+var SubFunc = require("./sub");
+
+var DivFunc = require("./dividir");
+
+var MultiFunc = require("./multi");
+
+
+console.log(SomaFunc(2,3));
+console.log(SubFunc(10,1));
+console.log(DivFunc(6,2));
+console.log(MultiFunc(2,122));
